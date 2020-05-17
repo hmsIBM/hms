a424bc7950b3713469269fc545541288cf2e3e04
shubham1803 <shubhamjee183@gmail.com>
2020-05-18 00:02:26 +0530

    shubham commit

--- HospitalApp/src/main/java/com/ibm/HospitalApp/repos/AppointmentRepo.java
+++ HospitalApp/src/main/java/com/ibm/HospitalApp/repos/AppointmentRepo.java
@@ -1,10 +1,16 @@
 package com.ibm.HospitalApp.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ibm.HospitalApp.entities.Appointment;
 
 
 public interface AppointmentRepo extends JpaRepository<Appointment, Integer>{
 	public Appointment findByName(String Name);
+	public List<Appointment> findAll();
+//	@Query("select u.* from Appointment u where u.ho = ?1")
+//	public List<Appointment> findAllAppointmentsInAHospital(String hospitalName );
 }
