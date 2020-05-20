51784bc37b67f66562959336c407973e8600e12f
yashmks <yashmks1998@gmail.com>
2020-05-20 11:25:58 +0530

    yhaysh

--- hospital-management-system/src/main/java/com/ibm/hospitalmanagementsystem/repos/TransactionalRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.hospitalmanagementsystem.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ibm.hospitalmanagementsystem.entities.TransactionalClass;
-
-public interface TransactionalRepo extends JpaRepository<TransactionalClass, Integer>{
-
-}
