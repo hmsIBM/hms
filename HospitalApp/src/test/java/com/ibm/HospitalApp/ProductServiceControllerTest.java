4fddda0a884752325ecf44436c41680ad961970f
yashmks1998 <39986456+yashmks1998@users.noreply.github.com>
2020-05-26 23:29:26 +0530

    Update ProductServiceControllerTest.java

--- HospitalApp/src/test/java/com/ibm/HospitalApp/ProductServiceControllerTest.java
+++ HospitalApp/src/test/java/com/ibm/HospitalApp/ProductServiceControllerTest.java
@@ -18,20 +18,20 @@ public class ProductServiceControllerTest extends AbstractTest {
   public void setUp() {
      super.setUp();
   }
-   @Test
-   public void getProductsList() throws Exception {
-      String uri = "/api/hospital";
-      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+//    @Test
+//    public void getProductsList() throws Exception {
+//       String uri = "/api/hospital";
+//       MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+//          .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
       
-      int status = mvcResult.getResponse().getStatus();
-      assertEquals(200, status);
-      String content = mvcResult.getResponse().getContentAsString();
-      Hospital[] hl = super.mapFromJson(content, Hospital[].class);
-      System.out.println("Digvijay singh rawat"+hl);
-//      Product[] productlist = super.mapFromJson(content, Product[].class);
-      assertTrue(hl.length> 0);
-   }
+//       int status = mvcResult.getResponse().getStatus();
+//       assertEquals(200, status);
+//       String content = mvcResult.getResponse().getContentAsString();
+//       Hospital[] hl = super.mapFromJson(content, Hospital[].class);
+//       System.out.println("Digvijay singh rawat"+hl);
+// //      Product[] productlist = super.mapFromJson(content, Product[].class);
+//       assertTrue(hl.length> 0);
+//    }
 //   @Test
 // public void createProduct() throws Exception {
 //    String uri = "/api/hospital/patient";
