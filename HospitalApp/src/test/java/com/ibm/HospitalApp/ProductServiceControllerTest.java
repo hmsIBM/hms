8f56e703670a4c6dfc6a9789c47211c91b50c7e0
yashmks <yashmks1998@gmail.com>
2020-05-26 23:25:25 +0530

    yash

--- HospitalApp/src/test/java/com/ibm/HospitalApp/ProductServiceControllerTest.java
+++ HospitalApp/src/test/java/com/ibm/HospitalApp/ProductServiceControllerTest.java
@@ -18,20 +18,20 @@ public class ProductServiceControllerTest extends AbstractTest {
   public void setUp() {
      super.setUp();
   }
-//   @Test
-//   public void getProductsList() throws Exception {
-//      String uri = "/api/hospital";
-//      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-//         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-//      
-//      int status = mvcResult.getResponse().getStatus();
-//      assertEquals(200, status);
-//      String content = mvcResult.getResponse().getContentAsString();
-//      Hospital[] hl = super.mapFromJson(content, Hospital[].class);
-//      System.out.println("Digvijay singh rawat"+hl);
-////      Product[] productlist = super.mapFromJson(content, Product[].class);
-//      assertTrue(hl.length> 0);
-//   }
+   @Test
+   public void getProductsList() throws Exception {
+      String uri = "/api/hospital";
+      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+      
+      int status = mvcResult.getResponse().getStatus();
+      assertEquals(200, status);
+      String content = mvcResult.getResponse().getContentAsString();
+      Hospital[] hl = super.mapFromJson(content, Hospital[].class);
+      System.out.println("Digvijay singh rawat"+hl);
+//      Product[] productlist = super.mapFromJson(content, Product[].class);
+      assertTrue(hl.length> 0);
+   }
 //   @Test
 // public void createProduct() throws Exception {
 //    String uri = "/api/hospital/patient";
@@ -48,13 +48,13 @@ public class ProductServiceControllerTest extends AbstractTest {
 //    assertEquals(content, "Doctor is created successfully");
 // }
 //  @Test
-  public void updateProduct() throws Exception {
-  String uri = "/api/hospital/max hospital/department/saiyam/patient";
-  Patient p= new Patient();
-  p.setId(999);
-  p.setName("Shubham");
-  p.setGender("male");
-  p.setDisease("Lukemia");
-  p.setEmailId("shubham9@gmail.com");
-  }
+//  public void updateProduct() throws Exception {
+//  String uri = "/api/hospital/max hospital/department/saiyam/patient";
+//  Patient p= new Patient();
+//  p.setId(999);
+//  p.setName("Shubham");
+//  p.setGender("male");
+//  p.setDisease("Lukemia");
+//  p.setEmailId("shubham9@gmail.com");
+//  }
 }
